e403fe98fd957cbc444ca0d04b02b22a3d67e195
yuno96100 <cyo579@gmail.com>
2026-01-27 01:51:16 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -3,3 +3,27 @@ const def_Log = true;
 const MainRoomNmae = "LOL 실험실";
 const GameMaster = "시스템";
 const rootPath = "sdcard/LOL/";
+const fileNameList = { UserList:"Users/UserList.txt",
+                       CharactorList:"Charactor/CharactorList.txt",
+                       CharOption:"Charactor/CharOption.txt",
+                       RecordList:"Charactor/RecordList.txt",
+                       RelationList:"Charactor/RelationList.txt"};
+
+const DBNameList = { Species:"DB/Species.txt",
+                    WearItem:"DB/WearItem.txt",
+                    SundryItem:"DB/SundryItem.txt",
+                    FunctionalityItem:"DB/FunctionalityItem.txt",
+                    Monster:"DB/Monster.txt",
+                    Nation:"DB/Nation.txt",
+                    City:"DB/City.txt",
+                    Facility:"DB/Facility.txt",
+                    Field:"DB/Field.txt",
+                    Skill:"DB/Skill.txt",
+                    Menufacture:"DB/Menufacture.txt",
+                    Sale:"DB/Sale.txt",
+                    Skillsale:"DB/Skillsale.txt",
+                    Cc:"DB/CC.txt",
+                    Property:"DB/Property.txt"};
+
+const WearItem_Kind = {Far_Weapon:"fw", Near_Weapon:"nw", Magic_Weapon:"mw", Wear:"wr", Assis_Arrow:"aa",Assis_Shield:"as", TwoHand_Weaopn:"tw"};
+const ITEM_KIND = {WEARITEM:"WearItem", SUNDRYITEM:"SundryItem", FUNCTIONALITYITEM:"FunctionalityItem"};
