13b0017cb54779540d1cb5231a389a63caa52566
yuno96100 <cyo579@gmail.com>
2026-01-27 09:35:14 +0900

    Update Object.js

--- Object.js
+++ Object.js
@@ -1,1 +1,16 @@
+function clsUserInfo(_info) {
+    var m_id = _info.id;
+    var m_pwd = _info.pwd;
+    var m_key = _info.key;
 
+    return {
+        getID: function() { return m_id; },
+        getPW: function() { return m_pwd; },
+        getKey: function() { return m_key; },
+        toJson: function() {
+            return { id: m_id, pwd: m_pwd, key: m_key };
+        }
+    };
+}
+
+exports.clsUserInfo = clsUserInfo;
