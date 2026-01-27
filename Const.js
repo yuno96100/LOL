6f63c83882b0405a4da26df5767cd658f9f88dcf
yuno96100 <cyo579@gmail.com>
2026-01-27 10:09:47 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,12 +1,1 @@
-const Prefix = "."; 
-const MainRoomNmae = "LOL 실험실"; // 본인의 오픈채팅방 이름으로 수정
-const rootPath = "sdcard/LOL/";
 
-const fileNameList = { 
-    UserList: "Users/UserList.txt" 
-};
-
-exports.Prefix = Prefix;
-exports.MainRoomNmae = MainRoomNmae;
-exports.rootPath = rootPath;
-exports.fileNameList = fileNameList;
