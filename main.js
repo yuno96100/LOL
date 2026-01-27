8e6aafe9f64de1386d19330554a702fbc5ec4ff2
yuno96100 <cyo579@gmail.com>
2026-01-28 00:17:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
 
-    // [1] 정보 확인용 (에러 방지를 위해 변수 존재 여부 체크)
+    /* =========================
+       [1] 정보 확인용 명령어
+    ========================= */
     if (msg === ".정보") {
         var res = "[ 봇 실시간 인식 정보 ]\n\n";
-        res += "● 인식된 방 이름: " + room + "\n";
+        res += "● 방 이름: " + room + "\n";
         res += "● 보낸 사람: " + sender + "\n";
-        res += "● 단체톡 여부(isGroupChat): " + isGroupChat + "\n";
+        res += "● isGroupChat 값: " + isGroupChat + "\n";
+        res += "● room === sender 여부: " + (room === sender) + "\n";
         replier.reply(res);
         return;
     }
 
-    // [2] 명령어 분기 처리 (가장 확실한 방법)
-    // 방 이름에 "LOL" 또는 "실험실"이 포함되어 있거나, 설정된 이름과 같으면 메인방으로 인식
-    var isMainRoom = (room === "LOL 실험실") || (room.indexOf("LOL 실험실") !== -1);
+    /* =========================
+       [2] 채팅 타입 판별
+       ※ 오픈채팅 대응 핵심
+    ========================= */
+
+    // 개인톡 vs 단톡 (오픈채팅 포함)
+    var isGroup = (room !== sender);
+
+    // 메인 단톡방 판별 (이름 포함 방식)
+    var isMainRoom = isGroup && room.indexOf("LOL 실험실") !== -1;
+
+    /* =========================
+       [3] 명령어 분기
+    ========================= */
 
     if (isMainRoom) {
-        // 단체톡방에서 실행될 명령어
+        // 메인 단체톡 (오픈채팅 포함)
         MainCmd(room, msg, sender, replier);
+
+    } else if (isGroup) {
+        // 그 외 단체톡 (서브방, 테스트방 등)
+        GroupCmd(room, msg, sender, replier);
+
     } else {
-        // 그 외(개인톡 등)에서 실행될 명령어 (.등록, .로그인 등)
+        // 개인톡
         UserCmd(room, msg, sender, replier, imageDB);
     }
 }
