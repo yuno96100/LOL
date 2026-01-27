4bae9861bbcc663ca10dd1494ce0800996491fbd
yuno96100 <cyo579@gmail.com>
2026-01-27 23:51:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
 
-    // .정보 입력 시 즉시 실행 (설정값 참조 안 함)
+    // [수정된 테스트 로직]
     if (msg === ".정보") {
-        var res = "[ 봇 연결 상태 확인 ]\n\n";
-        res += "● 현재 방 이름: " + room + "\n";
+        var res = "[ 봇 실시간 인식 정보 ]\n\n";
+        res += "● 인식된 방 이름: " + room + "\n";
+        res += "● 단체톡 여부: " + (isGroupChat ? "단체톡(Group)" : "개인톡(1:1)") + "\n";
         res += "● 보낸 사람: " + sender + "\n";
-        res += "● 그룹 채팅 여부: " + (isGroupChat ? "예" : "아니오") + "\n";
-        res += "● 패키지명: " + packageName;
+        
+        // 만약 단체톡인데 방이름이 닉네임으로 나온다면, 
+        // 아래 '판정' 부분을 참고해서 Const.js를 수정해야 합니다.
+        res += "● 판정: " + (room === "현재나오는닉네임" ? "이 닉네임이 현재 방이름임" : "불일치");
         
         replier.reply(res);
-        return; 
+        return;
     }
-    
-    // 이 아래부터는 기존 로직을 실행하되, 
-    // Const.js의 변수가 없으면 에러가 날 수 있으니 체크가 필요합니다.
+
+    // [분기 로직 개선]
+    // 방 이름이 자꾸 닉네임으로 나온다면, Const.js의 MainRoomName을 
+    // .정보 쳤을 때 나오는 그 '닉네임'과 똑같이 적어주어야 메인방으로 인식합니다.
+    if (room === libConst.MainRoomName) {
+        MainCmd(room, msg, sender, replier);
+    } else {
+        UserCmd(room, msg, sender, replier, imageDB);
+    }
 }
