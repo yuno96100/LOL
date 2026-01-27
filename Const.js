76f97583b8ec962c85fec9662a3c2c6e8c977fed
yuno96100 <cyo579@gmail.com>
2026-01-27 09:34:28 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,1 +1,12 @@
+const Prefix = "."; 
+const MainRoomNmae = "LOL 실험실";
+const rootPath = "sdcard/LOL/";
 
+const fileNameList = { 
+    UserList: "Users/UserList.txt" 
+};
+
+exports.Prefix = Prefix;
+exports.MainRoomNmae = MainRoomNmae;
+exports.rootPath = rootPath;
+exports.fileNameList = fileNameList;
