88ef0c20cd458c19ad945111dfcb22af9faf3466
yuno96100 <cyo579@gmail.com>
2026-01-29 02:17:03 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,8 +1,9 @@
 // Const.js
 const Prefix = "."; 
 const MainRoomName = "LOL실험실"; 
+const ErrorLogRoom = "게임봇"; // 에러 메시지가 전송될 전용 방 이름
 const AdminName = "시스템"; 
-const Version = "1.1.2"; // 시스템 체계 정보 추가 버전
+const Version = "1.1.4"; // 에러 로그 전용 방 설정 추가
 
 const RootPath = "sdcard/LOL/";      
 const DBPath = RootPath + "DB/";     
@@ -11,6 +12,7 @@ function bridge() {
     return {
         Prefix: Prefix,
         MainRoomName: MainRoomName,
+        ErrorLogRoom: ErrorLogRoom,
         AdminName: AdminName,
         RootPath: RootPath,
         DBPath: DBPath,
