1735725ee97d6758f4dca5ad86f43101d209756d
yuno96100 <cyo579@gmail.com>
2026-01-29 08:05:19 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,9 +1,9 @@
 // Const.js
 const Prefix = "."; 
 const MainRoomName = "LOL실험실"; 
-const ErrorLogRoom = "게임봇"; // 에러 메시지가 전송될 전용 방 이름
+const ErrorLogRoom = "게임봇"; 
 const AdminName = "시스템"; 
-const Version = "1.1.4"; // 에러 로그 전용 방 설정 추가
+const Version = "1.1.5"; // LoginManager 모듈 로드 오류 수정
 
 const RootPath = "sdcard/LOL/";      
 const DBPath = RootPath + "DB/";     
