b6d782875f91fd0711971138bf0e992865b6dd5d
yuno96100 <cyo579@gmail.com>
2026-01-29 01:29:10 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -4,8 +4,8 @@ const MainRoomName = "LOL실험실";
 const AdminName = "방장닉네임"; 
 
 // 경로 설정
-const RootPath = "sdcard/LOL/";      // 모든 리소스의 최상위 경로
-const DBPath = RootPath + "DB/";     // 실제 데이터베이스(.json)가 저장될 곳
+const RootPath = "sdcard/LOL/";      
+const DBPath = RootPath + "DB/";     
 
 function bridge() {
     return {
@@ -14,7 +14,8 @@ function bridge() {
         AdminName: AdminName,
         RootPath: RootPath,
         DBPath: DBPath,
-        UserPath: DBPath + "Users/", // 유저 파일 저장용 세부 경로
-        Currency: "RP"
+        UserPath: DBPath + "Users/",
+        Currency: "RP",
+        Version: "1.0.0"
     };
 }
