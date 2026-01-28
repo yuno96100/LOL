a257d8e3ded68e3afe3b3b3cc913e1b8365fab80
yuno96100 <cyo579@gmail.com>
2026-01-29 01:53:34 +0900

    Update Object.js

--- Object.js
+++ Object.js
@@ -9,7 +9,10 @@ function bridge() {
                     name: _name, 
                     joinedDate: new Date().toLocaleDateString() 
                 },
-                status: { level: 1, rp: 0, exp: 0 },
+                status: { 
+                    level: 1, 
+                    exp: 0 
+                },
                 inventory: []
             };
         }
