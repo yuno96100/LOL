bc5da59f7178e3ff9a1f36b025c08539ba16835c
yuno96100 <cyo579@gmail.com>
2026-01-29 02:05:47 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,10 +1,9 @@
 // Const.js
 const Prefix = "."; 
 const MainRoomName = "LOL실험실"; 
-const AdminName = "시스템"; 
-const Version = "1.1.0"; // 화폐 시스템 제거 버전
+const AdminName = "방장닉네임"; 
+const Version = "1.1.1"; // 런타임 에러 방지 적용 버전
 
-// 경로 설정
 const RootPath = "sdcard/LOL/";      
 const DBPath = RootPath + "DB/";     
 
