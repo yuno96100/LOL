8216d0fc86dc69f28dc0d932524889c418d3bb1d
yuno96100 <cyo579@gmail.com>
2026-01-29 08:27:50 +0900

    Update DataBase.js

--- DataBase.js
+++ DataBase.js
@@ -1,15 +1,20 @@
-// DataBase.js
+// DataBase.js (v1.1.5)
 const libConst = Bridge.getScopeOf("Const.js").bridge();
 
 function bridge() {
-    java.io.File(libConst.UserPath).mkdirs(); 
+    // 폴더 생성 로직
+    var folder = new java.io.File(libConst.UserPath);
+    if (!folder.exists()) folder.mkdirs(); 
 
     return {
         saveUser: function(_id, _data) {
             try {
                 FileStream.write(libConst.UserPath + _id + ".json", JSON.stringify(_data, null, 4));
                 return true;
-            } catch (e) { return false; }
+            } catch (e) { 
+                Log.e("Save Error: " + e.message);
+                return false; 
+            }
         },
         loadUser: function(_id) {
             try {
@@ -18,7 +23,8 @@ function bridge() {
             } catch (e) { return null; }
         },
         isExisted: function(_id) {
-            return java.io.File(libConst.UserPath + _id + ".json").exists();
+            var file = new java.io.File(libConst.UserPath + _id + ".json");
+            return file.exists();
         }
     };
 }
