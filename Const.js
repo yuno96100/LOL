aa9470d2dc78fae45fe858e059756006bce4272c
yuno96100 <cyo579@gmail.com>
2026-01-28 23:01:54 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,3 +1,15 @@
 // Const.js
-const MainRoomName = "LOL실험실"; // 'GameRoom'에서 변경
-const rootPath = "sdcard/LOL/";    // 'Kmjbot'에서 변경
+const Prefix = "."; // 명령어 접두사
+const MainRoomName = "LOL실험실"; // 게임이 작동할 메인 단체톡방 이름
+const AdminName = "방장닉네임"; // 1:1 대화를 유도할 관리자/시스템 닉네임
+
+function bridge() {
+    return {
+        Prefix: Prefix,
+        MainRoomName: MainRoomName,
+        AdminName: AdminName,
+        // 나중에 확장될 명칭들
+        Currency: "RP",
+        Version: "1.0.0"
+    };
+}
