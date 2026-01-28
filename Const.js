e1833a965f1ef74e1500bee0c15b9701836bd7e7
yuno96100 <cyo579@gmail.com>
2026-01-29 02:11:41 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,8 +1,8 @@
 // Const.js
 const Prefix = "."; 
 const MainRoomName = "LOL실험실"; 
-const AdminName = "시스템"; // "방장닉네임"에서 "시스템"으로 변경
-const Version = "1.1.1"; 
+const AdminName = "시스템"; 
+const Version = "1.1.2"; // 시스템 체계 정보 추가 버전
 
 const RootPath = "sdcard/LOL/";      
 const DBPath = RootPath + "DB/";     
