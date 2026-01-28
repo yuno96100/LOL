72a29f7a889997dc84c189e286561495471669cb
yuno96100 <cyo579@gmail.com>
2026-01-29 00:55:37 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,15 +1,20 @@
 // Const.js
-const Prefix = "."; // 명령어 접두사
-const MainRoomName = "LOL실험실"; // 게임이 작동할 메인 단체톡방 이름
-const AdminName = "방장닉네임"; // 1:1 대화를 유도할 관리자/시스템 닉네임
+const Prefix = "."; 
+const MainRoomName = "LOL실험실"; 
+const AdminName = "방장닉네임"; 
+
+// 경로 설정
+const RootPath = "sdcard/LOL/";      // 모든 리소스의 최상위 경로
+const DBPath = RootPath + "DB/";     // 실제 데이터베이스(.json)가 저장될 곳
 
 function bridge() {
     return {
         Prefix: Prefix,
         MainRoomName: MainRoomName,
         AdminName: AdminName,
-        // 나중에 확장될 명칭들
-        Currency: "RP",
-        Version: "1.0.0"
+        RootPath: RootPath,
+        DBPath: DBPath,
+        UserPath: DBPath + "Users/", // 유저 파일 저장용 세부 경로
+        Currency: "RP"
     };
 }
