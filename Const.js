bd7681f4cfcb948f810437a076d1b779a69c1d3d
yuno96100 <cyo579@gmail.com>
2026-01-29 12:49:23 +0900

    Const.js 업데이트

--- Const.js
+++ Const.js
@@ -2,7 +2,7 @@
 const Prefix = "."; 
 const MainRoomName = "LOL실험실"; 
 const ErrorLogRoom = "게임봇"; 
-const Version = "1.3.0"; 
+const Version = "1.3.1"; // 백업, 롤백, 초기화 및 삭제 기능 추가
 
 const RootPath = "sdcard/LOL/";      
 const DBPath = RootPath + "DB/";     
@@ -15,6 +15,7 @@ function bridge() {
         RootPath: RootPath,
         DBPath: DBPath,
         UserPath: DBPath + "Users/",
+        BackupPath: DBPath + "Backup/", // [추가] 백업 폴더 경로
         AdminPath: DBPath + "Admins.json",
         Version: Version
     };
