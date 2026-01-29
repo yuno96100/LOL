2041fbd1481cd857efb00bcdc7575dd2f1ea361c
yuno96100 <cyo579@gmail.com>
2026-01-30 02:00:58 +0900

    Update DataBase.js

--- DataBase.js
+++ DataBase.js
@@ -2,6 +2,7 @@ const libConst = Bridge.getScopeOf("Const.js").bridge();
 
 function bridge() {
     return {
+        // 유저 데이터 쓰기
         writeUser: function(id, data) {
             try {
                 var folder = new java.io.File(libConst.UserPath);
@@ -10,9 +11,13 @@ function bridge() {
                 return FileStream.write(path, JSON.stringify(data, null, 4));
             } catch (e) { return false; }
         },
+
+        // LoginManager 호환용 별칭
         saveUser: function(id, data) {
             return this.writeUser(id, data);
         },
+
+        // 유저 데이터 읽기
         readUser: function(id) {
             var path = libConst.UserPath + id + ".json";
             try {
@@ -21,9 +26,14 @@ function bridge() {
                 return JSON.parse(FileStream.read(path));
             } catch (e) { return null; }
         },
+
+        // 파일 존재 여부 확인
         isExisted: function(id) {
+            if (!id) return false;
             return new java.io.File(libConst.UserPath + id + ".json").exists();
         },
+
+        // 전체 유저 ID 목록 (파일명에서 추출)
         getUserList: function() {
             var folder = new java.io.File(libConst.UserPath);
             var files = folder.listFiles();
@@ -37,20 +47,31 @@ function bridge() {
             }
             return list;
         },
+
+        // 유저 삭제 (Backup 폴더로 이동)
         deleteUser: function(id) {
             try {
                 var fromFile = new java.io.File(libConst.UserPath + id + ".json");
                 var toFile = new java.io.File(libConst.BackupPath + id + ".json");
                 var backupFolder = new java.io.File(libConst.BackupPath);
                 if (!backupFolder.exists()) backupFolder.mkdirs();
-                return fromFile.renameTo(toFile);
+                
+                if (fromFile.exists()) {
+                    return fromFile.renameTo(toFile);
+                }
+                return false;
             } catch (e) { return false; }
         },
+
+        // 유저 복구 (Backup에서 Users로 이동)
         restoreUser: function(id) {
             try {
                 var fromFile = new java.io.File(libConst.BackupPath + id + ".json");
                 var toFile = new java.io.File(libConst.UserPath + id + ".json");
-                return fromFile.renameTo(toFile);
+                if (fromFile.exists()) {
+                    return fromFile.renameTo(toFile);
+                }
+                return false;
             } catch (e) { return false; }
         }
     };
