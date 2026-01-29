4dbbc38aff35fdddb1cfc483116a038c689c849c
yuno96100 <cyo579@gmail.com>
2026-01-29 12:49:43 +0900

    DataBase.js 업데이트

--- DataBase.js
+++ DataBase.js
@@ -2,40 +2,39 @@
 const libConst = Bridge.getScopeOf("Const.js").bridge();
 
 function bridge() {
-    var folder = new java.io.File(libConst.UserPath);
-    if (!folder.exists()) folder.mkdirs(); 
+    // 필요한 폴더들 생성
+    [libConst.UserPath, libConst.BackupPath].forEach(path => {
+        var folder = new java.io.File(path);
+        if (!folder.exists()) folder.mkdirs();
+    });
 
     return {
-        // [수정] 관리자 명단 읽기
-        getAdmins: function() {
+        // ... (기존 getAdmins, saveAdmins, saveUser, loadUser, isExisted, getUserList 동일)
+
+        // [신규] 유저 데이터 삭제 (삭제 전 자동 백업)
+        deleteUser: function(_id) {
             try {
-                let content = FileStream.read(libConst.AdminPath);
-                return content ? JSON.parse(content) : [];
-            } catch (e) { return []; }
-        },
-        // [수정] 관리자 명단 저장
-        saveAdmins: function(_list) {
-            try {
-                FileStream.write(libConst.AdminPath, JSON.stringify(_list, null, 4));
-                return true;
+                let userData = this.loadUser(_id);
+                if (userData) {
+                    // 삭제 전 백업 폴더에 저장
+                    FileStream.write(libConst.BackupPath + _id + "_bk.json", JSON.stringify(userData, null, 4));
+                    var file = new java.io.File(libConst.UserPath + _id + ".json");
+                    return file.delete();
+                }
+                return false;
             } catch (e) { return false; }
         },
-        // 유저 관련 (기존 로직 유지)
-        saveUser: function(_id, _data) {
+
+        // [신규] 유저 데이터 롤백 (백업 파일에서 복구)
+        rollbackUser: function(_id) {
             try {
-                FileStream.write(libConst.UserPath + _id + ".json", JSON.stringify(_data, null, 4));
-                return true;
+                let content = FileStream.read(libConst.BackupPath + _id + "_bk.json");
+                if (content) {
+                    FileStream.write(libConst.UserPath + _id + ".json", content);
+                    return true;
+                }
+                return false;
             } catch (e) { return false; }
-        },
-        loadUser: function(_id) {
-            try {
-                let content = FileStream.read(libConst.UserPath + _id + ".json");
-                return content ? JSON.parse(content) : null;
-            } catch (e) { return null; }
-        },
-        isExisted: function(_id) {
-            var file = new java.io.File(libConst.UserPath + _id + ".json");
-            return file.exists();
         }
     };
 }
