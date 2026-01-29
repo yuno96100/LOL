2cc9a6d9e01534f9c6e43b612fa2ad06ce73eec4
yuno96100 <cyo579@gmail.com>
2026-01-30 02:49:02 +0900

    Update LoginManager.js

--- LoginManager.js
+++ LoginManager.js
@@ -1,18 +1,16 @@
 function bridge() {
     return {
         tryRegister: function(id, pw, nick, DB, Obj) {
-            if (DB.isExisted(id)) return { success: false, msg: "❌ 이미 가입된 카카오톡 닉네임입니다." };
+            if (DB.isExisted(id)) return { success: false, msg: "❌ 이미 가입된 닉네임입니다." };
             var newUser = Obj.getNewUser(id, pw.trim(), nick);
             if (DB.saveUser(id, newUser)) {
-                return { success: true, msg: "✅ 가입 성공! [" + nick + "]님 환영합니다." };
+                return { success: true, msg: "✅ 가입 성공! [" + nick + "]님 환영합니다.\n🔑 ID: " + id };
             }
-            return { success: false, msg: "❌ 가입 처리 중 오류가 발생했습니다." };
+            return { success: false, msg: "❌ 가입 중 오류 발생." };
         },
         tryLogin: function(id, pw, DB) {
             var userData = DB.loadUser(id);
             if (!userData) return { success: false, msg: "등록되지 않은 유저입니다." };
-            
-            // 데이터의 비번과 입력받은 비번을 모두 trim 처리하여 대조
             if (String(userData.info.pw).trim() === String(pw).trim()) {
                 return { success: true, msg: "성공", data: userData };
             }
