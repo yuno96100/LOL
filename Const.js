2403ef7ba6a6dd71166d00347e5e05d07db39f42
yuno96100 <cyo579@gmail.com>
2026-01-29 12:47:39 +0900

    Const.js 업데이트

--- Const.js
+++ Const.js
@@ -2,7 +2,7 @@
 const Prefix = "."; 
 const MainRoomName = "LOL실험실"; 
 const ErrorLogRoom = "게임봇"; 
-const Version = "1.2.8"; // 로그인 세션 유지 로직 강화 통합본
+const Version = "1.3.0"; 
 
 const RootPath = "sdcard/LOL/";      
 const DBPath = RootPath + "DB/";     
