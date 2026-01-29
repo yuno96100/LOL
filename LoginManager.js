8bb2ada408207dbfdc7205a32b41f9ac68a8d95c
yuno96100 <cyo579@gmail.com>
2026-01-30 02:42:25 +0900

    Update LoginManager.js

--- LoginManager.js
+++ LoginManager.js
@@ -1,8 +1,8 @@
 function bridge() {
     return {
         tryRegister: function(id, pw, nick, DB, Obj) {
-            if (DB.isExisted(id)) return { success: false, msg: "❌ 이미 가입된 계정입니다." };
-            var newUser = Obj.getNewUser(id, pw, nick);
+            if (DB.isExisted(id)) return { success: false, msg: "❌ 이미 가입된 카카오톡 닉네임입니다." };
+            var newUser = Obj.getNewUser(id, pw.trim(), nick);
             if (DB.saveUser(id, newUser)) {
                 return { success: true, msg: "✅ 가입 성공! [" + nick + "]님 환영합니다." };
             }
@@ -10,9 +10,13 @@ function bridge() {
         },
         tryLogin: function(id, pw, DB) {
             var userData = DB.loadUser(id);
-            if (!userData) return { success: false, msg: "존재하지 않는 아이디입니다." };
-            if (userData.info.pw !== pw) return { success: false, msg: "비밀번호가 일치하지 않습니다." };
-            return { success: true, msg: "성공", data: userData };
+            if (!userData) return { success: false, msg: "등록되지 않은 유저입니다." };
+            
+            // 데이터의 비번과 입력받은 비번을 모두 trim 처리하여 대조
+            if (String(userData.info.pw).trim() === String(pw).trim()) {
+                return { success: true, msg: "성공", data: userData };
+            }
+            return { success: false, msg: "비밀번호가 일치하지 않습니다." };
         }
     };
 }
