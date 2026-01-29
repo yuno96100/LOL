f6a400ed61ac05f4ce131b071facf31bd5495875
yuno96100 <cyo579@gmail.com>
2026-01-30 01:30:46 +0900

    Update Const.js

--- Const.js
+++ Const.js
@@ -1,11 +1,11 @@
 const Prefix = "."; 
-const Version = "1.7.2"; 
+const Version = "1.8.1"; 
 
 function bridge() {
     return {
         Prefix: Prefix,
-        MainRoomName: "소환사의협곡",        // 그룹톡 이름 변경
-        ErrorLogRoom: "소환사의협곡관리",    // 관리자 방 이름 변경
+        MainRoomName: "소환사의협곡",
+        ErrorLogRoom: "소환사의협곡관리",
         RootPath: "sdcard/LOL/",
         DBPath: "sdcard/LOL/DB/",
         UserPath: "sdcard/LOL/DB/Users/",
