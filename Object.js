691c51f4f11c58dd47ad678156c91de138e186b7
yuno96100 <cyo579@gmail.com>
2026-01-29 14:06:10 +0900

    Update Object.js

--- Object.js
+++ Object.js
@@ -3,15 +3,19 @@ function bridge() {
     return {
         getNewUser: function(_id, _pw, _name) {
             return {
-                info: { 
-                    id: _id, 
-                    pw: _pw, 
-                    name: _name, 
-                    joinedDate: new Date().toLocaleDateString() 
+                info: {
+                    id: _id,
+                    pw: _pw,
+                    name: _name,
+                    title: "브론즈",
+                    joinDate: new Date().toLocaleString()
                 },
-                status: { 
-                    level: 1, 
-                    exp: 0 
+                status: {
+                    level: 1,
+                    exp: 0,
+                    money: 1000,
+                    win: 0,
+                    loss: 0
                 },
                 inventory: []
             };
