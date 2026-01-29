17d6d9d4a9d0b80897980157e790912e377e28c2
yuno96100 <cyo579@gmail.com>
2026-01-29 12:53:57 +0900

    DataBase.js 업데이트

--- DataBase.js
+++ DataBase.js
@@ -2,21 +2,75 @@
 const libConst = Bridge.getScopeOf("Const.js").bridge();
 
 function bridge() {
-    // 필요한 폴더들 생성
-    [libConst.UserPath, libConst.BackupPath].forEach(path => {
-        var folder = new java.io.File(path);
-        if (!folder.exists()) folder.mkdirs();
-    });
+    // 자동 폴더 생성 로직
+    var userFolder = new java.io.File(libConst.UserPath);
+    var backupFolder = new java.io.File(libConst.BackupPath);
+    if (!userFolder.exists()) userFolder.mkdirs(); 
+    if (!backupFolder.exists()) backupFolder.mkdirs(); 
 
     return {
-        // ... (기존 getAdmins, saveAdmins, saveUser, loadUser, isExisted, getUserList 동일)
+        // 관리자 명단 가져오기
+        getAdmins: function() {
+            try {
+                let content = FileStream.read(libConst.AdminPath);
+                return content ? JSON.parse(content) : [];
+            } catch (e) { return []; }
+        },
 
-        // [신규] 유저 데이터 삭제 (삭제 전 자동 백업)
+        // 관리자 명단 저장하기
+        saveAdmins: function(_list) {
+            try {
+                FileStream.write(libConst.AdminPath, JSON.stringify(_list, null, 4));
+                return true;
+            } catch (e) { return false; }
+        },
+
+        // 유저 데이터 저장
+        saveUser: function(_id, _data) {
+            try {
+                FileStream.write(libConst.UserPath + _id + ".json", JSON.stringify(_data, null, 4));
+                return true;
+            } catch (e) { return false; }
+        },
+
+        // 유저 데이터 로드
+        loadUser: function(_id) {
+            try {
+                let content = FileStream.read(libConst.UserPath + _id + ".json");
+                return content ? JSON.parse(content) : null;
+            } catch (e) { return null; }
+        },
+
+        // 유저 존재 여부 확인
+        isExisted: function(_id) {
+            var file = new java.io.File(libConst.UserPath + _id + ".json");
+            return file.exists();
+        },
+
+        /**
+         * [핵심] 유저 목록 가져오기 (main 72라인 에러 해결)
+         */
+        getUserList: function() {
+            try {
+                let folder = new java.io.File(libConst.UserPath);
+                let files = folder.listFiles();
+                let list = [];
+                if (files != null) {
+                    for (let i = 0; i < files.length; i++) {
+                        if (files[i].isFile() && files[i].getName().endsWith(".json")) {
+                            list.push(files[i].getName().replace(".json", ""));
+                        }
+                    }
+                }
+                return list;
+            } catch (e) { return []; }
+        },
+
+        // 유저 삭제 (삭제 전 자동 백업)
         deleteUser: function(_id) {
             try {
                 let userData = this.loadUser(_id);
                 if (userData) {
-                    // 삭제 전 백업 폴더에 저장
                     FileStream.write(libConst.BackupPath + _id + "_bk.json", JSON.stringify(userData, null, 4));
                     var file = new java.io.File(libConst.UserPath + _id + ".json");
                     return file.delete();
@@ -25,7 +79,7 @@ function bridge() {
             } catch (e) { return false; }
         },
 
-        // [신규] 유저 데이터 롤백 (백업 파일에서 복구)
+        // 유저 롤백
         rollbackUser: function(_id) {
             try {
                 let content = FileStream.read(libConst.BackupPath + _id + "_bk.json");
