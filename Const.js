732fa33996a5e30c73cd61a0509973c9311322e8
yuno96100 <cyo579@gmail.com>
2026-01-29 12:29:29 +0900

    Const.js 업데이트

--- Const.js
+++ Const.js
@@ -2,7 +2,7 @@
 const Prefix = "."; 
 const MainRoomName = "LOL실험실"; 
 const ErrorLogRoom = "게임봇"; 
-const Version = "1.2.4"; // 모듈 간 독립성 강화를 위한 DI 패턴 완성
+const Version = "1.2.6"; // 모듈 참조 안정화 버전
 
 const RootPath = "sdcard/LOL/";      
 const DBPath = RootPath + "DB/";     
