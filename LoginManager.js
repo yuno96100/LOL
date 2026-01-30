7e087c50a4f85442574645cc6dea719569aa5ec0
yuno96100 <cyo579@gmail.com>
2026-01-30 12:37:04 +0900

    LoginManager.js 업데이트

--- LoginManager.js
+++ LoginManager.js
@@ -1,17 +1,15 @@
 function bridge() {
     return {
         tryRegister: function(id, nick, DB, Obj) {
-            if (DB.isExisted(id)) return { success: false, msg: "❌ 이미 가입된 유저입니다." };
+            if (DB.isExisted(id)) return { success: false, msg: "❌ 이미 가입된 계정입니다." };
             var newUser = Obj.getNewUser(id, "0", nick);
-            if (DB.saveUser(id, newUser)) {
-                return { success: true, msg: "✅ 가입 성공! [" + nick + "]님 환영합니다." };
-            }
-            return { success: false, msg: "❌ 가입 중 오류가 발생했습니다." };
+            if (DB.writeUser(id, newUser)) return { success: true, msg: "✅ 가입 성공! [" + nick + "]님 환영합니다." };
+            return { success: false, msg: "❌ 가입 처리 중 오류 발생." };
         },
         tryLogin: function(id, DB) {
-            var userData = DB.readUser(id); 
-            if (!userData) return { success: false, msg: "가입 정보가 없습니다." };
-            return { success: true, msg: "성공", data: userData };
+            var data = DB.readUser(id);
+            if (!data) return { success: false, msg: "해당 닉네임의 가입 정보를 찾을 수 없습니다." };
+            return { success: true, data: data };
         }
     };
 }
