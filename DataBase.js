dec9bb0c59333676040b1446fff0984e20e809a7
yuno96100 <cyo579@gmail.com>
2026-01-31 21:41:01 +0900

    Update DataBase.js

--- DataBase.js
+++ DataBase.js
@@ -1,44 +1,37 @@
-const libConst = Bridge.getScopeOf("Const.js").bridge();
-
+/* ============================================================
+   [SECTION] 데이터 입출력 (File I/O)
+   ============================================================ */
 function bridge() {
+    var C = Bridge.getScopeOf("Const.js").bridge();
+    
+    // 폴더 체크 및 생성 함수
+    var checkFolder = function(path) {
+        var folder = new java.io.File(path);
+        if (!folder.exists()) folder.mkdirs();
+    };
+
     return {
-        isExisted: function(id) {
-            return new java.io.File(libConst.UserPath + id + ".json").exists();
+        readUser: function(name) {
+            try { return JSON.parse(FileStream.read(C.DataPath + name + ".json")); }
+            catch(e) { return null; }
         },
-        writeUser: function(id, data) {
-            try {
-                var folder = new java.io.File(libConst.UserPath);
-                if (!folder.exists()) folder.mkdirs();
-                FileStream.write(libConst.UserPath + id + ".json", JSON.stringify(data, null, 4));
-                return true;
-            } catch (e) { return false; }
+        writeUser: function(name, data) {
+            checkFolder(C.DataPath);
+            FileStream.write(C.DataPath + name + ".json", JSON.stringify(data, null, 4));
         },
-        readUser: function(id) {
-            try {
-                var path = libConst.UserPath + id + ".json";
-                if (!new java.io.File(path).exists()) return null;
-                return JSON.parse(FileStream.read(path));
-            } catch (e) { return null; }
+        deleteUser: function(name) {
+            return FileStream.remove(C.DataPath + name + ".json");
         },
         getAllUserIds: function() {
-            var folder = new java.io.File(libConst.UserPath);
-            if (!folder.exists()) folder.mkdirs();
-            var files = folder.list();
+            checkFolder(C.DataPath);
+            var files = new java.io.File(C.DataPath).listFiles();
             var list = [];
-            if (files) {
-                for (var i = 0; i < files.length; i++) {
-                    if (files[i].endsWith(".json")) list.push(files[i].replace(".json", ""));
+            for (var i in files) { 
+                if (files[i].isFile() && files[i].getName().endsWith(".json")) {
+                    list.push(files[i].getName().replace(".json", ""));
                 }
             }
             return list;
-        },
-        deleteUser: function(id) {
-            try {
-                var from = new java.io.File(libConst.UserPath + id + ".json");
-                var toFolder = new java.io.File(libConst.BackupPath);
-                if (!toFolder.exists()) toFolder.mkdirs();
-                return from.renameTo(new java.io.File(libConst.BackupPath + id + ".json"));
-            } catch (e) { return false; }
         }
     };
 }
