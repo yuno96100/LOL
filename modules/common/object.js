56e2772a7ddf7778909c23ddf5651effce65e0ab
yuno96100 <cyo579@gmail.com>
2026-02-02 23:57:04 +0900

    Update object.js

--- modules/common/object.js
+++ modules/common/object.js
@@ -1,1 +1,21 @@
-
+function bridge() {
+    return {
+        getNewUser: function(id, name) {
+            return {
+                info: {
+                    id: id,
+                    name: name,
+                    joinDate: new Date().getTime()
+                },
+                status: {
+                    level: 1,
+                    money: 1000,
+                    exp: 0,
+                    hp: 100,
+                    maxHp: 100
+                },
+                inventory: []
+            };
+        }
+    };
+}
