4df099b2b0758659903e7807b4da121f9c2676cc
yuno96100 <cyo579@gmail.com>
2026-02-03 00:08:25 +0900

    Update object.js

--- modules/common/object.js
+++ modules/common/object.js
@@ -1,19 +1,18 @@
 function bridge() {
     return {
-        getNewUser: function(id, name) {
+        // 계정 전용 객체
+        getNewAccount: function(id, name) {
             return {
-                info: {
-                    id: id,
-                    name: name,
-                    joinDate: new Date().getTime()
-                },
-                status: {
-                    level: 1,
-                    money: 1000,
-                    exp: 0,
-                    hp: 100,
-                    maxHp: 100
-                },
+                id: id,      // 고유 식별자
+                name: name,  // 닉네임
+                isBanned: false
+            };
+        },
+        // 게임 데이터 전용 객체
+        getNewProfile: function(name) {
+            return {
+                name: name,
+                status: { level: 1, money: 1000, exp: 0, hp: 100 },
                 inventory: []
             };
         }
