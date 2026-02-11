48b99591f157582e7ad4f156e1ef7d6ecd8f9dd1
yuno96100 <cyo579@gmail.com>
2026-02-12 01:15:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var Config = {
     LINE_CHAR: "━", 
     WRAP_LIMIT: 18, 
     DIVIDER_LINE: 14,
-    NAV_LEFT: "  ", 
+    NAV_LEFT: "  ", 
     NAV_RIGHT: " ", 
     NAV_ITEMS: ["⬅️이전", "❌취소", "🏠메뉴"]
 };
